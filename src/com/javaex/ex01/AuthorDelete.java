598d6f2eb59198cea879d58176694fe153a1cd29
subintoto <subintoto@naver.com>
2022-05-21 22:25:16 +0900

    author 0520

--- src/com/javaex/ex01/AuthorDelete.java
+++ src/com/javaex/ex01/AuthorDelete.java
@@ -3,7 +3,6 @@ package com.javaex.ex01;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AuthorDelete {
@@ -13,59 +12,48 @@ public class AuthorDelete {
 		// 0. import java.sql.*;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		//ResultSet rs = null;
+		
 		try {
 			// 1. JDBC 드라이버 (Oracle) 로딩
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-
 			
 			// 2. Connection 얻어오기
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
+			
 			
 			// 3. SQL문 준비 / 바인딩 / 실행
-			// 준비
+			//SQL문 준비
 			String query = "";
-			// query += " select book_id";
-			query += " select  title, ";
-			query += " 	    	pubs, ";
-			query += " 	    	pub_date, ";
-			query += "				author_id ";
-			query += " from book ";
-
+			query += " delete from author ";
+			query += " where author_id = ? ";
 			
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-
+			System.out.println(query);
+			//바인딩
+			pstmt = conn.prepareStatement(query); //문자열 쿼리로 만들기
+			pstmt.setInt(1, 5);
+						
 			
-			// 실행
-			rs = pstmt.executeQuery();
-
+			//실행
+			int count = pstmt.executeUpdate(); //쿼리문 실행
+			
 			
 			// 4.결과처리
-			while (rs.next()) {
-				// int bookId = rs.getInt("book_id");
-				String title = rs.getString("title");
-				String pubs = rs.getString("pubs");
-				String pubDate = rs.getString("pub_date");
-				String authorId = rs.getString("author_id");
-
-				System.out.println(title + "," + pubs + "," + pubDate + "," + authorId);
-			}
-
+			System.out.println(count+"건이 삭제되었습니다.");
+			
 		} catch (ClassNotFoundException e) {
 			System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
-
+			
 			
 			// 5. 자원정리
 			try {
-				/*
-				 * if (rs != null) { rs.close(); }
-				 */
+				/*if (rs != null) {
+					rs.close();
+				}*/
 				if (pstmt != null) {
 					pstmt.close();
 				}
@@ -76,7 +64,5 @@ public class AuthorDelete {
 				System.out.println("error:" + e);
 			}
 		}
-
 	}
-
 }
